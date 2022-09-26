3c2423bcdadd44332f3a2bb20eeede15272540ce
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-26 00:33:24 +0000

    uploading working sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  sum = 0;
+  
+  array.forEach((item) => {
+  
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+    sum += item;
+    }
+  
+  });
+
+  return sum;
 }
 
 module.exports = sumItems;
